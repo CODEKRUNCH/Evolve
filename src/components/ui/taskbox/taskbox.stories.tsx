3147278d5befff9cfb5210cdb3fbfdc056a6ace6
CODEKRUNCH <awaisshahzadpk@protonmail.com>
2025-10-22 09:57:53 -0700

    TaskBox Storybook Data

--- src/components/ui/taskbox/taskbox.stories.tsx
+++ src/components/ui/taskbox/taskbox.stories.tsx
@@ -11,5 +11,14 @@ export default meta;
 type Story = StoryObj<typeof Taskbox>;
 
 export const Default: Story = {
-  args: {}
+  args: {
+    title: 'Somethingg',
+    description: 'Do this AS fast As Possible',
+    status: 'done',
+    category: 'Something',
+    priority: 'low',
+    dueDate: '15-10-2025',
+    comments: 2,
+    views: 2,
+  },
 };
